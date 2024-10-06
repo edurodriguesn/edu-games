8e55da180032b45b9becd592d8a3760e8370ba12
Eduardo Rodrigues <edu.rodrigues.ap@gmail.com>
2024-10-05 21:37:59 -0300

    .

---
+++ src/appConfig.js
@@ -0,0 +1,25 @@
+// src/appConfig.js
+const express = require('express');
+const session = require('express-session');
+const path = require('path');
+const appConfig = (app) => {
+    //middleware para processar o corpo da requisição
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+
+    //configuração de sessão
+    app.use(session({
+        secret: 'secreto123',
+        resave: false,
+        saveUninitialized: true,
+    }));
+
+    //configuração para EJS
+    app.set('view engine', 'ejs');
+    app.set('views', path.join(__dirname, '../views')); //ajuste o caminho para as views
+
+    //middleware para servir arquivos estáticos
+    app.use(express.static(path.join(__dirname, '../public'))); //ajuste o caminho para a pasta pública
+};
+
+module.exports = appConfig;
--- src/rotasAdmin.js
+++ src/rotasAdmin.js
@@ -1,17 +1,10 @@
 const express = require('express'); //importa o módulo Express
 const router = express.Router(); //cria um objeto Router do Express, que permite definir rotas
 const db = require('./db'); //importa o módulo db (banco de dados)
-<<<<<<< HEAD
 const { usuarioAutenticado } = require('./rotasLogin'); //middleware de autenticação
 
 router.get('/admin', usuarioAutenticado, (req, res) => {
     const sortField = req.query.sort === 'data_modificacao' ? 'data_modificacao' : 'data_criacao'; //ordenar por data de criação como padrão
-=======
-const { isAuthenticated } = require('./rotasLogin'); // Middleware de autenticação
-
-router.get('/admin', isAuthenticated, (req, res) => {
-    const sortField = req.query.sort === 'data_modificacao' ? 'data_modificacao' : 'data_criacao'; // Ordenar por data de criação como padrão
->>>>>>> 6af0580dbd8d8a91935991d813e4ac762bd0fdbd
 
     const query = `SELECT id, nome, data_criacao, data_modificacao,slug FROM jogo ORDER BY ${sortField} DESC`;
 
@@ -24,13 +17,8 @@ router.get('/admin', isAuthenticated, (req, res) => {
     });
 });
 
-<<<<<<< HEAD
 //rota para excluir um jogo
 router.post('/admin/jogos/:id/delete', usuarioAutenticado, (req, res) => {
-=======
-// Rota para excluir um jogo
-router.post('/admin/jogos/:id/delete', isAuthenticated, (req, res) => {
->>>>>>> 6af0580dbd8d8a91935991d813e4ac762bd0fdbd
     const { id } = req.params;
 
     db.get("SELECT * FROM jogo WHERE id = ?", [id], (err, jogo) => {
@@ -38,39 +26,23 @@ router.post('/admin/jogos/:id/delete', isAuthenticated, (req, res) => {
             return res.status(500).send('Erro ao carregar o jogo');
         }
 
-<<<<<<< HEAD
         //verificar se o jogo foi encontrado
-=======
-        // Verificar se o jogo foi encontrado
->>>>>>> 6af0580dbd8d8a91935991d813e4ac762bd0fdbd
         if (!jogo) {
             return res.status(404).send('jogo não encontrado');
         }
 
-<<<<<<< HEAD
         //excluir o jogo do banco de dados
-=======
-        // Excluir o jogo do banco de dados
->>>>>>> 6af0580dbd8d8a91935991d813e4ac762bd0fdbd
         db.run("DELETE FROM jogo WHERE id = ?", [id], (err) => {
             if (err) {
                 return res.status(500).send('Erro ao excluir o jogo');
             }
 
-<<<<<<< HEAD
             //redirecionar para a página de administração
-=======
-            // Redirecionar para a página de administração
->>>>>>> 6af0580dbd8d8a91935991d813e4ac762bd0fdbd
             res.redirect('/admin');
         });
     });
 });
-<<<<<<< HEAD
 //rota para adicionar uma nova opção (plataforma, categoria ou desenvolvedor): OK!
-=======
-// Rota para adicionar uma nova opção (plataforma, categoria ou desenvolvedor): OK!
->>>>>>> 6af0580dbd8d8a91935991d813e4ac762bd0fdbd
 router.post('/add-option', (req, res) => {
     const { tipo, nome } = req.body;
 
@@ -89,11 +61,7 @@ router.post('/add-option', (req, res) => {
             return res.status(400).json({ success: false, message: 'Tipo inválido' });
     }
 
-<<<<<<< HEAD
     //verifica se a opção já existe antes de adicionar
-=======
-    // Verifica se a opção já existe antes de adicionar
->>>>>>> 6af0580dbd8d8a91935991d813e4ac762bd0fdbd
     db.get(`SELECT * FROM ${tableName} WHERE nome = ?`, [nome], (err, row) => {
         if (err) {
             return res.status(500).json({ success: false, message: 'Erro ao verificar a opção existente' });
@@ -103,11 +71,7 @@ router.post('/add-option', (req, res) => {
             return res.status(400).json({ success: false, message: 'A opção já existe' });
         }
 
-<<<<<<< HEAD
         //adiciona a nova opção se não existir
-=======
-        // Adiciona a nova opção se não existir
->>>>>>> 6af0580dbd8d8a91935991d813e4ac762bd0fdbd
         db.run(`INSERT INTO ${tableName} (nome) VALUES (?)`, [nome], (err) => {
             if (err) {
                 return res.status(500).json({ success: false, message: 'Erro ao adicionar a nova opção' });
