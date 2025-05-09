48ed2e12f1b8abcde001cd1ece25ee8b0ad89819
Eduardo Rodrigues <edu.rodrigues.ap@gmail.com>
2025-05-08 23:02:09 -0300

    fix port

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use((req, res) => {
 });
 
 //inicialização do servidor
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
